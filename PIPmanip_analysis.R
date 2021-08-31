2b994d3de98934dfd35774d8ddd1c2e06f938d06
Alex <alexandra.mitchell@ed.ac.uk>
2021-08-31 18:30:40 +0100

    Update PIPmanip_analysis.R
    Adding ID to analysis file

--- PIPmanip_analysis.R
+++ PIPmanip_analysis.R
@@ -16,12 +16,14 @@ library(Rmisc)
 DPath <- '/Users/alex/OneDrive - University of Edinburgh/Experiments/PIPTOT/Data'
 setwd(DPath) #Data path
 
-EXP <- read.csv('AGM_pilot1.csv') #reading in experimental data
+EXP <- read.csv('Pilot_JATOS.csv') #reading in experimental data
 #DEMO <- read.csv() #reading in demographic data
 
 ##### DATA WRANGLE #####
 ## WILL NEED EDITING WHEN IT COMES TO REAL DATA
-EXP <- EXP[, c(7,12:47,49:50,55,58:59,111,140,157:159,163,168,179,182,189,195,209,260,277)]
+EXP <- EXP[, 
+           c(7,12:47,49:50,55,58:59,111,140,157:160,163,168,179,182,189,195,209,260,277)
+           ]
 
 # defining trials
 EXP$trial_type <- factor(EXP$trial_type)
@@ -38,7 +40,11 @@ GO <- GO[GO$correct == 1 ,]
 # plotting
 GO$sound <- factor(GO$sound)
 ggplot(GO) +
-  geom_density(aes(response_time, colour = sound))
+  geom_density(aes(response_time, colour = sound)) +
+  facet_wrap(~jatosStudyResultId)
 
 # summary
-RT_GO <- summarySEwithin(data = GO, measurevar = 'response_time', withinvars = 'sound')
+RT_GO <- summarySEwithin(data = GO, measurevar = 'response_time', 
+                         withinvars = 'sound', betweenvars = 'jatosStudyResultId')
+
+
