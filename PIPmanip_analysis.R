56291aa1806fc30c3f1ff1067126046803cd6e05
Alex <alexandra.mitchell@ed.ac.uk>
2021-09-01 16:17:25 +0100

    STOP
    Changing to 70%
    Other finegrained task details

--- PIPmanip_analysis.R
+++ PIPmanip_analysis.R
@@ -13,7 +13,8 @@ library(ggpubr)
 library(Rmisc)
 
 # getting files
-DPath <- '/Users/alex/OneDrive - University of Edinburgh/Experiments/PIPTOT/Data'
+DPath <- 'C:/Users/amitch17/OneDrive - University of Edinburgh/Experiments/PIPTOT/Data'
+#DPath <- '/Users/alex/OneDrive - University of Edinburgh/Experiments/PIPTOT/Data'
 setwd(DPath) #Data path
 
 EXP <- read.csv('Pilot_JATOS.csv') #reading in experimental data
@@ -21,9 +22,6 @@ EXP <- read.csv('Pilot_JATOS.csv') #reading in experimental data
 
 ##### DATA WRANGLE #####
 ## WILL NEED EDITING WHEN IT COMES TO REAL DATA
-EXP <- EXP[, 
-           c(7,12:47,49:50,55,58:59,111,140,157:160,163,168,179,182,189,195,209,260,277)
-           ]
 
 # defining trials
 EXP$trial_type <- factor(EXP$trial_type)
@@ -40,11 +38,11 @@ GO <- GO[GO$correct == 1 ,]
 # plotting
 GO$sound <- factor(GO$sound)
 ggplot(GO) +
-  geom_density(aes(response_time, colour = sound)) +
-  facet_wrap(~jatosStudyResultId)
+  geom_density(aes(response_time, colour = sound), size = 1) +
+  facet_wrap(~vol)
 
 # summary
 RT_GO <- summarySEwithin(data = GO, measurevar = 'response_time', 
-                         withinvars = 'sound', betweenvars = 'jatosStudyResultId')
+                         withinvars = 'sound', betweenvars = 'vol')
 
 
