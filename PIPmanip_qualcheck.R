34b5dd3fb7db660b846976c73054b22143f4c228
Alex <alexandra.mitchell@ed.ac.uk>
2021-09-16 16:51:59 +0100

    quality check
    Creating code to check quality of prolific data

---
+++ PIPmanip_qualcheck.R
@@ -0,0 +1,48 @@
+##### DATA QUALITY CHECK FOR PIPTOT MANIPULATION CHECK #####
+## CREATED BY A.G.MITCHELL - 16.09.21
+## ADAPTED FROM RTanalysis.R originally created by H Stocks & R D McIntosh
+
+##### SET UP #####
+#loading packages
+library(psych)
+library(tidyverse)
+library(ggplot2)
+library(pwr)
+library(reshape2)
+library(ggpubr)
+library(Rmisc)
+
+# getting files
+#DPath <- 'C:/Users/amitch17/OneDrive - University of Edinburgh/Experiments/PIPTOT/Data'
+DPath <- '/Users/alex/OneDrive - University of Edinburgh/Experiments/PIPTOT/Data'
+setwd(DPath) #Data path
+
+EXP <- read.csv('PIPmanip_JATOS.csv') #reading in experimental data
+DEMO <- read.csv('PIPmanip_QUAL.csv') #reading in demographic data
+
+##### DATA WRANGLE #####
+## WILL NEED EDITING WHEN IT COMES TO REAL DATA
+
+# defining trials
+EXP$trial_type <- factor(EXP$trial_type)
+levels(EXP$trial_type) <- c('catch','no','go')
+
+# grouping
+CATCH <- EXP[EXP$trial_type == 'catch' ,]
+GO <- EXP[EXP$trial_type == 'go' ,]
+NOGO <- EXP[EXP$trial_type == 'no' ,]
+
+# removing incorrect
+GO <- GO[GO$correct == 1 ,]
+
+# plotting
+GO$sound <- factor(GO$sound)
+ggplot(GO) +
+  geom_density(aes(response_time_keyboard_response, colour = sound), size = 1) +
+  facet_wrap(~jatosStudyResultId)
+
+# summary
+RT_GO <- summarySEwithin(data = GO, measurevar = 'response_time_keyboard_response', 
+                         withinvars = 'sound', betweenvars = 'jatosStudyResultId')
+RT_med <- aggregate(response_time_keyboard_response ~ sound*jatosStudyResultId, median, data = GO)
+
